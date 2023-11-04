1b1588d8d52d1c61921d296198a81a10f8c5fbf8
ccConstant <celine_constant@hotmail.fr>
2023-11-04 18:06:34 +0100

    corrections for using breeze

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('country');
-            $table->date('birth_date');
-            $table->enum('gender',  ['Woman', 'Man', 'Other']);
+            $table->string('country')->nullable();
+            $table->date('birth_date')->nullable();
+            $table->enum('gender',  ['Woman', 'Man', 'Other'])->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,19 +1,23 @@
-import('./bootstrap');
+import './bootstrap';
+import '../css/app.css';
 
-window.Vue = import('vue').default;
-import { createApp } from 'vue'
-import App from './App.vue'
-import { createRouter,createWebHashHistory} from 'vue-router' ; 
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
-const router = new createRouter({
-    history: createWebHashHistory(),
-    routes: [{
-        path: '/',
-        name: 'home',
-        component: import('./App.vue').default
-    }
-    ]
-})
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-createApp(App).mount('#app');
-
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue, Ziggy)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
